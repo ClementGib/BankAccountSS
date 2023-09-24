aecd0925b27f3fb2eace481dd1a0b2f8536424fd
Clement Gibert <c.gibertpro@gmail.com>
2023-09-24 23:31:25 +0200

    Multimodule init: in progress...

---
+++ bank-account-spring/application/src/main/java/com/cdx/bas/application/scheduler/SchedulerImpl.java
@@ -0,0 +1,23 @@
+package com.cdx.bas.application.scheduler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@SpringBootApplication
+@EnableScheduling
+public class SchedulerImpl implements Scheduler {
+
+    Logger logger = LoggerFactory.getLogger(SchedulerImpl.class);
+
+    @Override
+    @Scheduled(fixedRate = 5000)
+    public void processQueue() {
+        logger.info("Scheduler start");
+
+
+        logger.info("Scheduler end");
+    }
+}
