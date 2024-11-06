77f53c52870afa021e4e660ae163478c99940484
“Clems” <c.gibertpro@gmail.com>
2024-11-06 01:30:45 +0100

    Spark first test with CSV and parquet

--- application/src/main/java/com/cdx/bas/application/scheduler/SchedulerImpl.java
+++ application/src/main/java/com/cdx/bas/application/scheduler/SchedulerImpl.java
@@ -1,6 +1,8 @@
 package com.cdx.bas.application.scheduler;
 
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,12 +11,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import java.io.Serializable;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
 @EnableScheduling
 public class SchedulerImpl implements Scheduler, Serializable {
-    String path = "application/src/test/resources/bs.csv";
-
+    String filePath = getClass().getClassLoader().getResource("bs.csv").getPath();
     Logger logger = LoggerFactory.getLogger(SchedulerImpl.class);
 
     @Override
@@ -25,8 +28,12 @@ public class SchedulerImpl implements Scheduler, Serializable {
                 .master("local[*]")
                 .config("spark.ui.enabled", "false")
                 .getOrCreate();
-//        Dataset<Row> bsDf = spark.read().csv(path);
-//        bsDf.show();
+        Dataset<Row> bsDf = spark.read()
+                .option("delimiter", ";")
+                .option("header", "true")
+                .csv(filePath);
+        bsDf.show();
+        bsDf.write().parquet("bs.parquet");
         logger.info("Scheduler end");
     }
 }
